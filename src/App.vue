235809532d532b64b688fe75466bed2ee5eb439f
MarcoTapia <marckantonio@ciencias.unam.mx>
2021-06-03 10:35:02 -0500

    agregando footer a las pantallas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,61 @@
 <template>
   <div id="app">
-    <router-view/>
+    <link rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+          integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+          crossorigin="anonymous">
+    <Header/>
+    <router-view></router-view>
+    <Footer/>
   </div>
 </template>
+
+<script>
+  import Footer from './Footer';
+  //import Header from './Header';
+  
+  export default {
+    name: "App",
+    components: {
+      'Footer' : Footer,
+    //  'Header': Header
+    }
+  }
+</script>
+
+<style>
+html,body {
+  height:100%;
+}
+#app {
+  position: relative;
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+#contenedor {
+  width: 100%;
+  background-color: transparent;
+  padding-bottom: 60px;
+  padding-top: 20px;
+}
+.centra {
+  margin: auto;
+  padding-top: 40px;
+  padding-bottom: 40px;
+}
+.loader {
+  position: fixed;
+  top:0px;
+  right:0px;
+  width:100%;
+  height:100%;
+  background-color:#ffffff;
+  background-repeat:no-repeat;
+  background-position:center;
+  z-index:10000000;
+  opacity: 0.4;
+  filter: alpha(opacity=40);
+}
+</style>
---
+++ src/Footer.vue
@@ -0,0 +1,120 @@
+<template>
+  <div id="foot">
+    <footer class="page-footer font-small stylish-color-dark pt-4 absolute">
+      <div class="container py-2">
+        <div class="row">
+          <div class="col-sm-1"></div>
+          <div class="col-sm-4">
+
+            <div class="row my-4">
+              <div class="col-sm-2 my-auto">
+                <i class="fas fa-map-marker-alt fa-lg" style="padding-top:10%;"></i>
+              </div>
+              <div class="col my-auto">
+                <div class="row">
+                  Blvd. Adolfo López Mateos #172
+                </div>
+                <div class="row">
+                  Piso 8, Colonia Merced Gómez
+                </div>
+                <div class="row">
+                  Delegación Benito Juárez, CP 03930
+                </div>
+                <div class="row">
+                  <b>Ciudad de México, MÉXICO</b>
+                </div>
+              </div>
+            </div>
+            <div class="row my-4">
+              <div class="col-sm-2 my-auto">
+                <i class="fas fa-phone fa-lg" style="padding-top: 10%"></i>
+              </div>
+              <div class="col my-auto">
+                <div class="row">
+                  <b>+(52) 55 5593 3942</b>
+                </div>
+              </div>
+            </div>
+            <div class="row my-4">
+              <div class="col-sm-2 my-auto">
+                <i class="fas fa-envelope fa-lg" style="padding-top: 10%"></i>
+              </div>
+              <div class="col my-auto">
+                <div class="row">
+                  <a href="#"><b>hola@petstore.ci.ultrasist.net</b></a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-sm-2"></div>
+
+          <div class="col-sm-4 text-left">
+            <div class="row my-4">
+              <b>Sobre Nosotros</b>
+            </div>
+            <div class="row my-4">
+              <p></p>
+            </div>
+            <div class="row my-4">
+              <div class="col-sm-3">
+                <a href="#">
+                <span class="social">
+                  <i class="fab fa-twitter-square fa-2x"></i>
+                </span>
+                </a>
+              </div>
+              <div class="col-sm-3">
+                <a href="#">
+                <span class="social">
+                  <i class="fab fa-facebook-square fa-2x"></i>
+                </span>
+                </a>
+              </div>
+              <div class="col-sm-3">
+                <a href="#">
+                <span class="social">
+                   <i class="fab fa-github-square fa-2x"></i>
+                </span>
+                </a>
+
+              </div>
+              <div class="col-sm-3">
+                <a href="#">
+                <span class="social">
+                  <i class="fab fa-linkedin fa-2x"></i>
+                </span>
+                </a>
+              </div>
+            </div>
+
+          </div>
+          <div class="col-sm-1"></div>
+        </div>
+
+      </div>
+      <div class="footer-copyright text-center py-3 darker">© 2021 Derechos reervados</div>
+
+    </footer>
+  </div>
+
+</template>
+
+<style>
+.stylish-color-dark{
+  background-color: rgba(3, 14, 31, 0.93);
+  color: #e7e7c9;
+}
+.darker{
+  background-color: #030718FF;
+}
+.social{
+  color:rgba(248,248,189,0.89);
+}
+#foot{
+  position: static;
+  width: 100%;
+  bottom: 0;
+}
+</style>
