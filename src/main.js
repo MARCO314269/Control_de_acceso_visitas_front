b7d679c444295d4b633b2889541a7e674e8e9ebd
MarcoTapia <marckantonio@ciencias.unam.mx>
2021-06-04 15:27:48 -0500

    agregando componente vuelidate

--- src/main.js
+++ src/main.js
@@ -13,6 +13,8 @@ import VueRouter from 'vue-router'
 import VModal from 'vue-js-modal'
 import router from './router'
 import store from './store'
+import Vuelidate from "vuelidate"
+
 
 /*Vue.config.productionTip = false*/
 
@@ -21,6 +23,8 @@ Vue.use(VueRouter, router)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(VueAxios, axios)
+Vue.use(Vuelidate);
+
 
 /*axios.defaults.baseURL = process.env.VUE_APP_URL*/
 
